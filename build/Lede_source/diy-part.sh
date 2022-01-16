735388ab9bbb1d22dcac7b8ae3dd2a890d06329b
GWen124 <9708281@qq.com>
2022-01-16 15:38:55 +0800

    Update

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -36,7 +36,7 @@ EOF
 sed -i 's/\"services\"/\"control\"/g' feeds/luci/applications/luci-app-wol/luasrc/controller/wol.lua
 
 # 修改插件名字
-echo 'Modify default name in menu'
+echo 'Modify default name in system menu'
 #system menu
 sed -i 's/"Web 管理"/"Web管理"/g' `grep "Web 管理" -rl ./`
 sed -i 's/"管理权"/"权限管理"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
@@ -47,7 +47,7 @@ sed -i 's/"启动项"/"启动管理"/g' feeds/luci/modules/luci-base/po/zh-cn/ba
 sed -i 's/"软件包"/"软件管理"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"TTYD 终端"/"命令终端"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/"Argon 主题设置"/"主题设置"/g' `grep "Argon 主题设置" -rl ./`
-
+echo 'Modify default name in control menu'
 #others
 sed -i 's/"网络存储"/"存储"/g' `grep "网络存储" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-turboacc/po/zh-cn/turboacc.po
@@ -56,31 +56,32 @@ sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -r
 sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
 sed -i 's/"在线用户"/"在线设备"/g' package/GWen124/luci-app-onliner/luasrc/controller/onliner.lua
 #sed -i 's/"autoipsetadder"/"自动设置IP"/g' `grep "autoipsetadder" -rl ./`
-
+echo 'Modify default name in services menu'
 #services menu
 sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' `grep "阿里云盘 WebDAV" -rl ./`
 sed -i 's/京东签到服务/京东签到/g' package/lean/luci-app-jd-dailybonus/luasrc/controller/jd-dailybonus.lua
 sed -i 's/"DDNS.to内网穿透"/"DDNSTO"/g' `grep "DDNS.to内网穿透" -rl ./`
 sed -i 's/"解锁网易云灰色歌曲"/"网易音乐"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 sed -i 's/天翼家庭云\/云盘提速/天翼云盘/g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
-#sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
 sed -i 's/"UU游戏加速器"/"UU加速器"/g' `grep "UU游戏加速器" -rl ./`
 sed -i 's/"AdGuard Home"/"AdHome"/g' `grep "AdGuard Home" -rl ./`
 sed -i 's/"Frp 内网穿透"/"Frp客户端"/g' `grep "Frp 内网穿透" -rl ./`
 sed -i 's/UU游戏加速器/UU加速器/g' package/lean/luci-app-uugamebooster/po/zh-cn/uuplugin.po
 sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' package/GWen124/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 sed -i 's/msgstr "UPnP"/msgstr "UPnP服务"/g' feeds/luci/applications/luci-app-upnp/po/zh-cn/upnp.po
-#sed -i 's/"广告屏蔽大师 Plus+"/"Adb Plus+"/g' `grep "广告屏蔽大师 Plus+" -rl ./`
 sed -i 's/"Rclone"/"Rclone挂载"/g' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
-#sed -i 's/"WebGuide"/"网页导航"/g' package/GWen124/luci-app-webguide/luasrc/controller/webguide.lua
 sed -i 's/"Go 阿里云盘 WebDAV"/"阿里云盘"/g' `grep "Go 阿里云盘 WebDAV" -rl ./`
-sed -i 's/CPU占用率限制/CPU调节/g' package/GWen124/luci-app-cpulimit/po/zh_Hans/cpulimit.po
+sed -i 's/Hello World/VSSR/g'  package/GWen124/luci-app-vssr/luasrc/controller/vssr.lua
+sed -i 's/Adblock Plus+/Adb Plus+/g'  package/GWen124/luci-app-adblock-plus/luasrc/controller/adblock.lua
+#sed -i 's/CPU占用率限制/CPU调节/g' package/GWen124/luci-app-cpulimit/po/zh_Hans/cpulimit.po
+#sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
+#sed -i 's/"WebGuide"/"网页导航"/g' package/GWen124/luci-app-webguide/luasrc/controller/webguide.lua
 #sed -i 's/"iKoolProxy 滤广告"/"广告过滤"/g' package/GWen124/luci-app-ikoolproxy/luasrc/controller/koolproxy.lua
 #sed -i 's/"TelegramBot"/"电报机器人"/g'  package/GWen124/luci-app-telegrambot/luasrc/controller/telegrambot.lua
 sed -i 's/"Nezha Agent"/"哪吒面板"/g'  package/GWen124/luci-app-nezha/luasrc/controller/nezha-agent.lua
+#sed -i 's/"WebGuide"/"网页导航"/g'  package/GWen124/luci-app-webguide/luasrc/controller/webguide.lua
 #sed -i 's/"Webd 网盘"/"WebDisk"/g'  package/GWen124/luci-app-webd/po/zh-cn/webd.po
-
-
-
+echo 'Modify default name in network menu'
 #network
 sed -i 's/内网测速网页版/内网测速/g' package/GWen124/luci-app-speedtest-web/po/zh-cn/speedtest-web.po
+sed -i 's/"IP\/MAC绑定"/"地址绑定"/g' package/lean/luci-app-arpbind/po/zh-cn/arpbind.po
