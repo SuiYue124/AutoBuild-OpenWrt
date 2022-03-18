3cb8b3c5576126b1452c18bfe6a8e85867f903fe
GWen124 <9708281@qq.com>
2022-03-18 19:39:21 +0800

    Update

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -8,7 +8,7 @@
 # 选择argon为默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-curl -fsSL https://raw.githubusercontent.com/GWen124/Script/master/OpenWrt/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
+#curl -fsSL https://raw.githubusercontent.com/GWen124/Script/master/OpenWrt/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
 curl -fsSL https://raw.githubusercontent.com/GWen124/Script/master/OpenWrt/index.htm > ./package/lean/autocore/files/x86/index.htm
 
 # 增加个性名字${Author}默认为你的github账号
